23964910caca5df60d5d1df8300fb0195115e009
Manar0116 <145463363+Manar0116@users.noreply.github.com>
2023-11-04 23:42:57 +0100

    Add files via upload

---
+++ ex2td3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<vector>
+#include<iterator>
+void TriBull(vector<int>liste){
+    bool y=false
+    do{
+        
+       while(y) ;
+    }
+}
+using namespace std;
+
+int main()
+{ 
+    vector<int>list;
+    int value; 
+    bool x=true;
+    cout<<"entrer les valeurs de la liste :"<<endl;
+    while(x){
+       cin>>value; 
+       list.push_back(value);
+       cout<<"tu veux entrer d autre valeurs  si oui entrer 1 si non entrer 0: ";
+       cin>>x; }
+    
+
+return 0;}
